8a389798b7eae6e682fe0bebab0c2f4793a9561d
Ryo-M-Code <ryo.m.code@gmail.com>
2020-12-31 00:36:03 +0900

    [fix]ビルド時のtailwindのエラーを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    purge: [],
+    purge: ['./components/*', './lib/*', './pages/*', './tests/*'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},

e56601269091e6d8e9f93490e6975c7c3345602b
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-05 00:20:18 +0900

    [add]text-shadowのカスタムプロパティを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,5 +40,29 @@ module.exports = {
 
             addUtilities(contentUtilities, ['before', 'after']);
         }),
+        function ({ addUtilities }) {
+            const newUtilities = {
+                '.text-shadow': {
+                    textShadow: '0px 2px 3px black',
+                },
+                '.text-shadow-md': {
+                    textShadow: '0px 3px 3px black',
+                },
+                '.text-shadow-lg': {
+                    textShadow: '0px 5px 3px black',
+                },
+                '.text-shadow-xl': {
+                    textShadow: '0px 7px 3px black',
+                },
+                '.text-shadow-2xl': {
+                    textShadow: '0px 10px 3px black',
+                },
+                '.text-shadow-none': {
+                    textShadow: 'none',
+                },
+            };
+
+            addUtilities(newUtilities);
+        },
     ],
 };

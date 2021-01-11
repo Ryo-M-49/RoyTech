415ef4d38b833c3f58d14a4de8b13b835fd493fb
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-11 17:42:16 +0900

    [fix]アポストロフィを記号でエスケープ

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -17,7 +17,7 @@ const About: React.FC = () => {
                     </div>
                     <div className="w-5/6 sm:w-1/2 p-6">
                         <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-                            Hi There. I'm Ryo M
+                            Hi There. I&aposm Ryo M
                         </h3>
                         <p className="text-gray-600 mb-8 text-lg">
                             Born in 1995. A web developer based in Tokyo. I have

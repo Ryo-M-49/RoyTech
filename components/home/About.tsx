77ddf729512abeb7724e4e7d4056f2716f9c1dba
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-11 20:36:32 +0900

    [update]SEO上の問題点を修正

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -16,9 +16,9 @@ const About: React.FC = () => {
                         <img src="beer.png" alt="beer" />
                     </div>
                     <div className="w-5/6 sm:w-1/2 p-6">
-                        <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                        <h2 className="text-3xl text-gray-800 font-bold leading-none mb-3">
                             Hi There. I am Ryo M
-                        </h3>
+                        </h2>
                         <p className="text-gray-600 mb-8 text-lg">
                             Born in 1995. A web developer based in Tokyo. I have
                             always been very curious about new technologies.

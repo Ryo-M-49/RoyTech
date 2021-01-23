4e956b4e627605993f8749093fb3f9d115eaa12d
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-23 21:57:08 +0900

    変更テスト

--- pages/work.tsx
+++ pages/work.tsx
@@ -10,7 +10,7 @@ const Work: React.FC = () => {
             ogImage="https://user-images.githubusercontent.com/57619070/104796298-4ec56f80-57f8-11eb-81bb-8ad6a2676c70.png"
         >
             <div className="h-screen flex flex-col justify-center ">
-                <h1 className="text-4xl md:text-7xl text-center">
+                <h1 className="text-5xl md:text-7xl text-center">
                     We are<br></br>under construction.
                 </h1>
             </div>
